9c281ecb83c54e750ab67a836cacda499732ddc6
Judare85 <judare85@hotmail.com>
2023-02-24 01:51:18 -0500

    Tercer commit

--- src/pages/PokeInfo.jsx
+++ src/pages/PokeInfo.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../components/pokedex/Shared/header'
 import './styles/pokeInfo.css'
+import './styles/loading.css'
 
 
 const PokeInfo = () => {
@@ -12,15 +13,11 @@ const PokeInfo = () => {
   const { id } = useParams()
   const [hasError, setHasError] = useState(false)
   const [poke, setPoke] = useState()
-
-  const handleScrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    setIsLoading(true)
     axios.get(url)
       .then(res => {
         setPoke(res.data)
@@ -29,7 +26,10 @@ const PokeInfo = () => {
       .catch(err => {
         setHasError(true)
         console.log(err)
-      })
+        })
+      .finally(setTimeout(() => {
+            setIsLoading(false)
+          }, 2200))
   }, [id])
 
   console.log(poke)
@@ -37,103 +37,112 @@ const PokeInfo = () => {
 
   if (hasError) {
     return <div className='not_found-img'>
-              <h1 className='poke_not-found'>🥵I am sorry, the pokemon with name "{id}" was not found🥵</h1>
-          </div>
+      <h1 className='poke_not-found'>🥵I am sorry, the pokemon with name "{id}" was not found🥵</h1>
+    </div>
   } else {
     return (
-      <>
+      <div>
         <Header />
-        <header className={`info_poke img-${poke?.types[0].type.name}`}>
-          <img className='poke_info-img' src={poke?.sprites.other['official-artwork'].front_default}
-            alt='' />
-        </header>
-        <h1 className='poke_info-name'>{poke?.name}</h1>
-        <div className='container_poke-info'>
-          <div className='characteristics'>
-            <div className='poke_weight-height'>
-              <div className='poke_characteristics'>
-                <span>Weight</span>
+        {
+          isLoading ? <div className='container_gif'><img className='gif' src="/images/pokeball.gif" alt="" /></div>
+            : <div>
+              <header className={`info_poke img-${poke?.types[0].type.name}`}>
+                <img className='poke_info-img' src={poke?.sprites.other['official-artwork'].front_default}
+                  alt='' />
+              </header>
+              <h1 className='poke_info-name'>{poke?.name}</h1>
+              <div className='container_poke-info'>
+                <div className='characteristics'>
+                  <div className='poke_weight-height'>
+                    <div className='poke_characteristics'>
+                      <span>Weight</span>
 
-                <p>{poke?.weight}</p>
+                      <p>{poke?.weight}</p>
 
-              </div>
-              <div className='poke_characteristics'>
-                <span>Height</span>
-                <p>{poke?.height}</p>
-              </div>
-            </div>
-          </div>
-
-
-          <div className='type_ability'>
-            <div className='box_title'>
-              <h2>Types</h2>
-              <div className='types_abilities'>
-                {
-                  poke?.types.map(type => (
-                    <span className={`type-${poke?.types[0].type.name}`} key={type.type.url}>
-                      {type.type.name}
-                    </span>
-                  ))
-                }
-              </div>
-            </div>
-            <div className='box_title'>
-              <h2>Abilities</h2>
-              <div className='types_abilities'>
-                {
-                  poke?.abilities.map(ability => (
-                    <span key={ability.ability.url}>
-                      {ability.ability.name}
-                    </span>
-                  ))
-                }
-              </div>
-            </div>
-          </div>
-
-
-          <div className='stats'>
-            <h1 className='stats_title'>Stats</h1>
-            {
-              poke?.stats.map(stat => (
-                <div className='bar_stats' key={stat.stat.url}>
-                  <div className='bar_stat-text'>
-                    <span>{stat.stat.name}</span>
-                    <p>{stat.base_stat}/150</p>
-                  </div>
-                  <div className='bar' style={{ background: `linear-gradient(90deg, #E6901E 0, #FCD676 ${stat.base_stat}%, rgb(231, 231, 231) ${stat.base_stat}% 100%)` }}>
-
+                    </div>
+                    <div className='poke_characteristics'>
+                      <span>Height</span>
+                      <p>{poke?.height}</p>
+                    </div>
                   </div>
                 </div>
-              ))
-            }
-          </div>
-          
-            <header>
-            <h1 className='move_title'>Movements</h1>
-            </header>
-            <section className='moves_container'>
-            <ul className='movements'>
-            {
-              poke?.moves.slice(0,25).map(move => (
-                <li className='movements_info' key={move.move.url}>
-                   {move.move.name}
-                </li>
-              ))
-            }
-            </ul>
-          </section>
 
-          <footer>
-            <button className='return__pokedex-button' onClick={handleBackClick}>Back page</button>
-        <button className='scroll__button' onClick={handleScrollToTop}>Go top</button>
-        </footer> 
 
-        </div>
-      </>
+                <div className='type_ability'>
+                  <div className='box_title'>
+                    <h2>Types</h2>
+                    <div className='types_abilities'>
+                      {
+                        poke?.types.map(type => (
+                          <span className={`type-${poke?.types[0].type.name}`} key={type.type.url}>
+                            {type.type.name}
+                          </span>
+                        ))
+                      }
+                    </div>
+                  </div>
+                  <div className='box_title'>
+                    <h2>Abilities</h2>
+                    <div className='types_abilities'>
+                      {
+                        poke?.abilities.map(ability => (
+                          <span key={ability.ability.url}>
+                            {ability.ability.name}
+                          </span>
+                        ))
+                      }
+                    </div>
+                  </div>
+                </div>
+
+
+                <div className='stats'>
+                  <h1 className='stats_title'>Stats</h1>
+                  {
+                    poke?.stats.map(stat => (
+                      <div className='bar_stats' key={stat.stat.url}>
+                        <div className='bar_stat-text'>
+                          <span>{stat.stat.name}</span>
+                          <p>{stat.base_stat}/150</p>
+                        </div>
+                        <div className='bar' style={{ background: `linear-gradient(90deg, #E6901E 0, #FCD676 ${stat.base_stat}%, rgb(231, 231, 231) ${stat.base_stat}% 100%)` }}>
+
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+
+                <header>
+                  <h1 className='move_title'>Movements</h1>
+                </header>
+                <section className='moves_container'>
+                  <ul className='movements'>
+                    {
+                      poke?.moves.slice(0, 25).map(move => (
+                        <li className='movements_info' key={move.move.url}>
+                          {move.move.name}
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </section>
+
+              </div>
+            </div>
+        }
+
+      </div>
     )
   }
 }
-}
 export default PokeInfo
+
+
+
+
+
+
+
+
+
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -5,80 +5,81 @@ import { useNavigate } from 'react-router-dom'
 import PokeCard from '../components/PokeCard'
 import Header from '../components/pokedex/Shared/header'
 import SelectTypes from '../components/SelectTypes'
-import  './styles/pokedex.css'
+import './styles/pokedex.css'
+import './styles/loading.css'
 
 
 
 const Pokedex = () => {
 
-   const {nameTrainer} =useSelector(state=> state)
+  const { nameTrainer } = useSelector(state => state)
 
-   const [pokemons, setPokemons] = useState()
-   const [selectValue, setSelectValue] = useState('allpokemons')
-   const [isLoading, setIsLoading] = useState(false)
+  const [pokemons, setPokemons] = useState()
+  const [selectValue, setSelectValue] = useState('allpokemons')
+  const [isLoading, setIsLoading] = useState(false)
 
-   const handleScrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  };
 
-  const handleBackClick = () => {
-    window.history.back();
-  };
-  
-
-   useEffect(() => {
+  useEffect(() => {
 
     if (selectValue === 'allpokemons') {
       const url = 'https://pokeapi.co/api/v2/pokemon?limit=102&offset=0'
-    axios.get(url)
-    .then(res => setPokemons(res.data))
-    .catch(err => console.log(err))
-    }else{
+      setIsLoading(true)
+      axios.get(url)
+        .then(res => setPokemons(res.data))
+        .catch(err => console.log(err))
+        .finally(setTimeout(() => {
+          setIsLoading(false)
+        }, 1500))
+    } else {
+      setIsLoading(true)
       axios.get(selectValue)
-      .then(res =>{
-        const results= res.data.pokemon.map(e => e.pokemon)
-       setPokemons({results})
-      })
-      .catch(err => console.log(err))
+        .then(res => {
+          const results = res.data.pokemon.map(e => e.pokemon)
+          setPokemons({ results })
+        })
+        .catch(err => console.log(err))
+        .finally(setTimeout(() => {
+          setIsLoading(false)
+        }, 2200))
     }
-   }, [selectValue]);
+  }, [selectValue]);
 
 
-   const navigate= useNavigate()
-   
-   const handleSubmit= e => {
+  const navigate = useNavigate()
+
+  const handleSubmit = e => {
     e.preventDefault()
-    const inputValue= e.target.pokemon.value.trim().toLowerCase()
+    const inputValue = e.target.pokemon.value.trim().toLowerCase()
     navigate(`/pokedex/${inputValue}`)
-    e.target.pokemon.value= ''
-   }
+    e.target.pokemon.value = ''
+  }
 
   return (
     <div className='poke_main-container'>
-        <Header/>
+      <Header />
+      {
+        isLoading ? <div className='container_gif'><img className='gif' src="/images/pokeball.gif" alt="" /></div>
+        : <div>
         <h1 className='poke_greeting-pokedex'><span className='poke_greeting-span'>Hi {nameTrainer}</span>, here you can find your favourite pokemon.</h1>
-          <form  className='poke_container' onSubmit={handleSubmit}>
-            <input className='poke_input' placeholder='search a pokemon' id='pokemon' type="text" />
-            <button className='pokedex_search'>Search</button>
-          </form>
-          <SelectTypes setSelectValue= {setSelectValue}/>
-            <div className='poke_cards'>
-            {
-                pokemons?.results.map(pokemon => (
-                    <PokeCard
-                    key={pokemon.url}
-                    pokemon={pokemon}
-                    />
-                ))
-            }
-            </div> 
-            <footer>
-            <button className='return__pokedex-button' onClick={handleBackClick}>Back page</button>
-        <button className='scroll__button' onClick={handleScrollToTop}>Go top</button>
-        </footer> 
+        <form className='poke_container' onSubmit={handleSubmit}>
+          <input className='poke_input' placeholder='search a pokemon' id='pokemon' type="text" />
+          <button className='pokedex_search'>Search</button>
+        </form>
+        <SelectTypes setSelectValue={setSelectValue} />
+        <div className='poke_cards'>
+          {
+            pokemons?.results.map(pokemon => (
+              <PokeCard
+                key={pokemon.url}
+                pokemon={pokemon}
+              />
+            ))
+          }
+        </div>
+      </div>
+
+      }
+      
     </div>
   )
 }
